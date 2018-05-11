e91ba3b39c41c3d871f9c8f00f7b08da4ee58b39
Darren Jones <darren@darrenjonesdesign.co.uk>
2018-05-11 13:30:50 +0100

    Update vim-setup.sh

--- vim-setup.sh
+++ vim-setup.sh
@@ -12,6 +12,14 @@ curl -LSso $HOME/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 echo "Installing nerdtree"
 git clone --quiet https://github.com/scrooloose/nerdtree.git $HOME/.vim/bundle/nerdtree
 
+# Install/update vim-airline
+echo "Installing vim-airline"
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+
+# Install/update ctrlp
+echo "Installing ctrlp"
+git clone https://github.com/kien/ctrlp.vim.git ~/.vim/bundle/ctrlp/ctrlp.vim
+
 # Backup vimrc
 echo "Backing up .vimrc"
 if [ -e $HOME/.vimrc ]; then
